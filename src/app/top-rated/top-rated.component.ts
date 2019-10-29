d875f22d7bd9b53bb91af72d07ef9e9119372062
zaheer <zaheermalik284@gmail.com>
2019-10-29 17:26:54 +0500

    comment and rating added

--- src/app/top-rated/top-rated.component.ts
+++ src/app/top-rated/top-rated.component.ts
@@ -10,7 +10,10 @@ import { async } from '@angular/core/testing';
 export class TopRatedComponent implements OnInit {
 
   data: any;
-  empty=false
+  empty = false
+  ratings: any;
+  onlyRating = []
+  comments: any;
   constructor(private api: ApiService) { }
 
   ngOnInit() {
@@ -21,15 +24,31 @@ export class TopRatedComponent implements OnInit {
     data.subscribe((res: any) => {
       this.data = res.resources;
       for (let i = 0; i < this.data.length; i++) {
-        let comments=res.comments.filter(c=>c.resource==this.data[i].id)
-        let ratings=res.ratings.filter(c=>c.resource==this.data[i].id)
-        this.data[i]={...this.data[i],comments:comments,ratings:ratings}
+        this.comments = res.comments.filter(c => c.resource == this.data[i].id)
+        this.ratings = res.ratings.filter(c => c.resource == this.data[i].id)
+        let twoComment=[]
+        if(this.comments.length>2){
+          twoComment.push(this.comments[0])
+          twoComment.push(this.comments[1])
+        }else{
+          twoComment=[...twoComment,this.comments]
+        }
+        this.data[i] = { ...this.data[i], comments: this.comments, fewComment: twoComment}
       }
+      for (let i = 0; i < this.data.length; i++) {
+        this.onlyRating = this.ratings.filter(r => r.rated_by != this.comments[i].commenter)
+      }
+      
       console.log(this.data)
       if (this.data.length == 0) {
         this.empty = true
       }
     })
+  }
+
+  getRating(rater) {
+    let rate:any=this.ratings.filter(r => r.rated_by == rater)
+    return rate[0].rating
   }
 
   saveLink = async (id) => {
